de63d391dbf03be8b2aac812ee98bbbe1203e7b5
billwanggithub <billwang.github@gmail.com>
2023-11-27 18:32:47 +0800

    Add guihelper

---
+++ GuiHelper.cs
@@ -0,0 +1,34 @@
+﻿using Serilog;
+using System;
+using System.Windows.Controls;
+
+namespace Helper
+{
+    public class GuiHelper
+    {
+        public static void InvokeOnUIThread(Control control, Action action)
+        {
+            if (control == null)
+                return;
+
+            try
+            {
+                if (control.Dispatcher.CheckAccess())
+                {
+                    control.Dispatcher.Invoke(action);
+                }
+                else
+                {
+                    action();
+                }
+            }
+            catch (Exception ex)
+            {
+                // log with exception here
+                Log.Error("===========exception error================");
+                Log.Error(ex.ToString());
+                Log.Error("==========================================");
+            }
+        }
+    }
+}
